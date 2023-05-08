1dec1ea2b96c93d9efe78166e17d0af88efba845
jcesarjunior <jcesarjunior@gmail.com>
2023-05-08 17:40:25 -0300

    Setup Inicial

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('teste222  ');
+import express from 'express'
+import hbs from 'express-handlebars'
+
+const app = express()
+
+import conn from './db/conn.js'
+
+conn
+    .sync()
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
